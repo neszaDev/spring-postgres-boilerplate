4320a7574af4c05fa2cf2f7eb5d4d6d175790ece
neszaDev <pattaradanai.pnes@gmail.com>
2026-09-27 01:01:16 +0700

    chore: remove BOM from AuthServiceRegisterConflictTest (utf8 no bom)

--- src/test/java/com/example/boilerplate/auth/AuthServiceRegisterConflictTest.java
+++ src/test/java/com/example/boilerplate/auth/AuthServiceRegisterConflictTest.java
@@ -1,4 +1,4 @@
-﻿package com.example.boilerplate.auth;
+package com.example.boilerplate.auth;
 
 import com.example.boilerplate.common.exception.ConflictException;
 import com.example.boilerplate.user.UserRepository;
